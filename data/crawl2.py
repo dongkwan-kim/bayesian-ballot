47e2b7226f16c5400a73935f5cc03d518323040b
Dongkwan Kim <todoaskit@gmail.com>
2018-06-06 14:50:10 +0900

    Clean up the files

--- data/crawl2.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-
-PATH = "/home/donggyun/Downloads/chromedriver"
-URL = "http://www.assembly.go.kr/assm/memact/congressman/memCond/memCond.do"
-
-driver = webdriver.Chrome(PATH)
